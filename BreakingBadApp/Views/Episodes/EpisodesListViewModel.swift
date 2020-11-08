fbd7275e100fd73adadabfd5dff13c29c3c0afc2
leandro <leandro.a.estrada@gmail.com>
2020-11-08 19:10:52 -0300

    Episode's screen working

--- BreakingBadApp/Views/Episodes/EpisodesListViewModel.swift
+++ BreakingBadApp/Views/Episodes/EpisodesListViewModel.swift
@@ -5,4 +5,27 @@
 //  Created by Leandro de Araujo Estrada on 08/11/20.
 //
 
-import Foundation
+import SwiftUI
+
+class EpisodesListViewModel: ObservableObject {
+    @Published var episodes: [Episode] = []
+
+    init() {
+        fetchData()
+    }
+
+    private func fetchData() {
+        let search = SearchAPIURLProvider(endpoint: .episodes)
+        
+        Requester.request(search: search) { [weak self] (result: Result<[Episode], APIError>) in
+            switch result {
+                case.success(let searchResult):
+                    DispatchQueue.main.async {
+                        self?.episodes = searchResult
+                    }
+                case.failure(let error):
+                    print(error)
+            }
+        }
+    }
+}
