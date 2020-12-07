77853c21091b6e84a4bbb0beec404b1ba8d99370
leandro <leandro.a.estrada@gmail.com>
2020-12-07 16:36:00 -0300

    UITest for cell and tabBar

--- BreakingBadAppUITests/BreakingBadAppUITests.swift
+++ BreakingBadAppUITests/BreakingBadAppUITests.swift
@@ -8,29 +8,39 @@
 import XCTest
 
 class BreakingBadAppUITests: XCTestCase {
+    
+    var app: XCUIApplication!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+    
+    func testTabBar() throws {
+        
+         app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+        let tabBar = app.tabBars["Tab Bar"].buttons["film"]
+        
+        let firstCell = app.tables.cells["Episode:, Pilot, Release date:, 01-20-2008,   Season:, 1"].staticTexts["Episode:"]
+        
+        if tabBar.isSelected {
+            XCTAssertTrue(firstCell.exists)
+        }
     }
-
+    
+    func testCell() throws {
+        // UI tests must launch the application that they test.
+        app = XCUIApplication()
+        app.launch()
+        
+        let tableButton = app.tables/*@START_MENU_TOKEN@*/.buttons["Walter White\nA.K.A. Heisenberg\nPresumed dead"]/*[[".cells[\"Walter White\\nA.K.A. Heisenberg\\nPresumed dead\"].buttons[\"Walter White\\nA.K.A. Heisenberg\\nPresumed dead\"]",".buttons[\"Walter White\\nA.K.A. Heisenberg\\nPresumed dead\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        
+        
+        let ocupationLabel = app.staticTexts["High School Chemistry Teacher"]
+        
+        if tableButton.isSelected {
+            XCTAssertTrue(ocupationLabel.exists)
+        }
+    }
+    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
